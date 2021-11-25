deaa90555436662312633d56729ff679aa3d2ae6
Kae-Yang Hsieh <gsunny4178@gmail.com>
2021-11-25 00:35:47 -0500

    fix: add husky sort, msd, lsd sort examples

--- src/main/java/playground.java
+++ src/main/java/playground.java
@@ -1,12 +1,46 @@
+import java.io.IOException;
 import java.text.Collator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import edu.neu.coe.info6205.sort.counting.MSDStringSort;
+import edu.neu.coe.info6205.sort.huskySort.PureHuskySort;
+import edu.neu.coe.info6205.sort.huskySort.utils.HuskyCoderFactory;
+
 
 public class playground {
 
     public static void main(String[] args) {
-        String words = "台北";
+        String[] originalString = {"台", "南", "美", "食", "最", "棒", "了", "好", "想", "逛", "水", "族", "館"};
+        String[] MSDString = originalString.clone();
+        MSDStringSort.sort(MSDString);
+        System.out.println("MSD String sort result: ");
+        for(String i: MSDString){
+            System.out.print(i);
+        }
+        System.out.println("");
+
+        String[] LSDString = originalString.clone();
+        MSDStringSort.sort(LSDString);
+        System.out.println("LSD String sort result: ");
+        for(String i: LSDString){
+            System.out.print(i);
+        }
+        System.out.println("");
+
+        //String[] HuskyString = {"g", "a", "m", "e", "o", "f", "t", "h", "r", "o", "n", "e"};
+        String[] HuskyString = originalString.clone();
+        PureHuskySort huskySort = new PureHuskySort(HuskyCoderFactory.asciiCoder, false, false);
+        huskySort.sort(HuskyString);
+        System.out.println("HuskySort String sort result: ");
+        for(String i: HuskyString){
+            System.out.print(i);
+        }
+        System.out.println("");
+
+        String words = String.join("", originalString);
+
+        System.out.println("");
 
         String pinyin = Utils.wordToPinyin(words, true);
         System.out.println("Pinyin: " + pinyin);
